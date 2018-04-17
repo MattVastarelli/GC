8d2f739edb726c9036caf1f00c7154d46b9adea4
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-04-17 19:23:04 -0400

    lab 1

---
+++ lab1.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+textvar = "text Var"
+print("Text: ", textvar)
+
+numVar = 6
+print("Number: ", numVar)
+
+print("Math function number: ", numVar, " factorial: ", math.factorial(numVar))
+
+randNum = random.randint(0,20)
+print("Random number: ", randNum, " Plus Number var = ", randNum + numVar)
