1e5b105d8ab48cb0dc3ae86aa73a01ad6bafd337
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-05-29 09:47:48 -0400

    pep8

--- Robot.py
+++ Robot.py
@@ -1,45 +1,50 @@
-from random import randint #for random
+# for random
+from random import randint
 
 print("Day 1 Program")
-counter = 0;
-var1 = "";
-stringList = ["Thank you for sharing ","That’s interesting ","Good to hear "]
+counter = 0
+var1 = ""
+stringList = ["Thank you for sharing ", "That’s interesting ", "Good to hear "]
 inFile = open("dataFile.txt")
-outFile = open("outFile.txt","w")
+outFile = open("outFile.txt", "w")
 
 print("Into python part")
-#while till quit
+# while till quit
 print("while loop")
 while var1 != "quit":
     var1 = input("enter input or quit to quit: ")
 
-    if var1 == "python": print("Gen Cyber")
-    else: print(var1)
+    if var1 == "python":
+        print("Gen Cyber")
+    else:
+        print(var1)
 
-#For 5
+# For 5
 print("for loop")
 for i in range(5):
     var1 = input("enter input: ")
     counter += 1
-    print(var1,"Counter: ", counter)
+    print(var1, "Counter: ", counter)
 
 print("Advanced python part")
 
-#while till quit
+# while till quit
 print("while loop")
 while var1 != "quit":
     var1 = input("enter input or quit to quit: ")
-    print(stringList[randint(0,2)], var1)
+    print(stringList[randint(0, 2)], var1)
 
-#file stuff
+# file stuff
+# read
+# strip
 print("Reading from file")
 with open("dataFile.txt") as f:
-    var1 = f.readlines()# read
-    var1 = [x.strip() for x in var1] #strip
+    var1 = f.readlines()
+    var1 = [x.strip() for x in var1]
+# reset counter
+counter = 0
 
-counter = 0 #reset counter
-
-#print convo
+# print convo
 print("print convo")
 for str in var1:
     print(stringList[randint(0, 2)], var1[counter])
@@ -47,10 +52,12 @@ for str in var1:
 
 counter = 0
 
-#print to file
+# print to file
 print("print to file")
-newLine = "" #hold line
-newParagraph = "" # hold more than one line
+# hold line
+newLine = ""
+# hold more than one line
+newParagraph = ""
 for str in var1:
     newLine = stringList[randint(0, 2)] + var1[counter]
     counter += 1
@@ -58,6 +65,6 @@ for str in var1:
 
 # write to the file
 outFile.write(newParagraph)
-#close the files
+# close the files
 inFile.close()
 outFile.close()
