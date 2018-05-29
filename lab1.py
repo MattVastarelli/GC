b3fb1abef36c0609c696b4fad817d769fe3af924
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-05-29 09:52:41 -0400

    comments

--- lab1.py
+++ lab1.py
@@ -1,13 +1,18 @@
+# for factorial
 import math
+# for randint
 import random
 
+# you could print anything with print
 textvar = "text Var"
 print("Text: ", textvar)
 
 numVar = 6
 print("Number: ", numVar)
 
+# you could call a fuction in a statment
 print("Math function number: ", numVar, " factorial: ", math.factorial(numVar))
 
-randNum = random.randint(0,20)
+# or do math
+randNum = random.randint(0, 20)
 print("Random number: ", randNum, " Plus Number var = ", randNum + numVar)
