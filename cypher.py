410867610f6173d60d906e6cd53926a43532d9f3
Matthew Vastarelli <30515659+MattVastarelli@users.noreply.github.com>
2018-05-28 20:03:58 -0400

    Added dictionary
    added a dictionary to shorten and simplify the code + pep8

--- cypher.py
+++ cypher.py
@@ -1,70 +1,29 @@
-#input string
-varInput = "There is something infantile in the presumption that somebody else has a responsibility to give " + \
-           "your life meaning and point… The truly adult view, by contrast, is that our life is as meaningful, " + \
-           "as full and as wonderful as we choose to make it."
+# Cypher as a dict
+dict = {
+    "a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t",
+    "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m",
+    "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f",
+    "v": "e", "w": "d", "x": "c", "y": "b", "z": "a",
+}
+
+# input string
+varInput = "There is something infantile in the presumption that" + \
+    "somebody else has a responsibility to give your life meaning " + \
+    "and point… The truly adult view, by contrast, is that our " + \
+    "life is as meaningful, as full and as wonderful as we choose to make it."
 # new string for substution cypher
 subCypher = ""
 
 print("Input prior to cypher: ", varInput)
 
-#substution cypher
-for char in varInput.lower():#lower to aviod error due to case
-    if char == "a":
-        subCypher += "z"
-    elif char == "b":
-        subCypher += "y"
-    elif char == "c":
-        subCypher += "x"
-    elif char == "d":
-        subCypher += "w"
-    elif char == "e":
-        subCypher += "v"
-    elif char == "f":
-        subCypher += "u"
-    elif char == "g":
-        subCypher += "t"
-    elif char == "h":
-        subCypher += "s"
-    elif char == "i":
-        subCypher += "r"
-    elif char == "j":
-        subCypher += "q"
-    elif char == "k":
-        subCypher += "p"
-    elif char == "l":
-        subCypher += "o"
-    elif char == "m":
-        subCypher += "n"
-    elif char == "n":
-        subCypher += "m"
-    elif char == "o":
-        subCypher += "l"
-    elif char == "p":
-        subCypher += "k"
-    elif char == "q":
-        subCypher += "j"
-    elif char == "r":
-        subCypher += "i"
-    elif char == "s":
-        subCypher += "h"
-    elif char == "t":
-        subCypher += "g"
-    elif char == "u":
-        subCypher += "f"
-    elif char == "v":
-        subCypher += "e"
-    elif char == "w":
-        subCypher += "d"
-    elif char == "x":
-        subCypher += "c"
-    elif char == "y":
-        subCypher += "b"
-    elif char == "z":
-        subCypher += "a"
+# substution cypher
+# lower to aviod error due to case
+for char in varInput.lower():
+    if char in dict:
+        subCypher += dict[char]
     else:
         subCypher += char
 
 print("\nSubstution Cyber: ", subCypher)
 
-#transpostion cypher
-
+# transpostion cypher
